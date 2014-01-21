bba563f817c830a36b48bc0a4563abe732436503
Oleksiy Dyagilev <Oleksiy_Dyagilev@epam.com>
2014-01-21 12:49:14 +0200

    empty maven project

---
+++ src/main/java/com/epam/Main.java
@@ -0,0 +1,10 @@
+package com.epam;
+
+/**
+ * Created by Oleksiy_Dyagilev
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
