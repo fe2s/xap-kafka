2f2e44fb2a0b46dc95b21cb2d1898bfbc9363075
Mykola Zalyayev <PSIXss@bigmir.net>
2014-01-24 12:54:56 +0200

    Add connection to MeSQL in consumer

--- example/consumer/src/main/java/com/epam/consumer/Consumer.java
+++ example/consumer/src/main/java/com/epam/consumer/Consumer.java
@@ -2,27 +2,86 @@ package com.epam.consumer;
 
 import kafka.consumer.ConsumerIterator;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.InitializingBean;
 
 import com.epam.openspaces.persistency.kafka.consumer.KafkaConsumer;
 import com.epam.openspaces.persistency.kafka.protocol.KafkaDataOperation;
+import com.gigaspaces.sync.SpaceSynchronizationEndpointException;
 
 public class Consumer implements InitializingBean {
 
     private KafkaConsumer consumer;
 
+    private SessionFactory sessionFactory;
+
     public void setConsumer(KafkaConsumer consumer) {
         this.consumer = consumer;
     }
 
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
+
+        consume();
+    }
+
+    private void consume() {
+
         ConsumerIterator<String, KafkaDataOperation> iterator = consumer
                 .getKafkaIterator("data");
 
         while (iterator.hasNext()) {
-            System.out.println(iterator.next().message());
+            Session session = getSessionFactory().openSession();
+            Transaction tr = session.beginTransaction();
+            try {
+
+                KafkaDataOperation dataOperation = iterator.next().message();
+
+                switch (dataOperation.getType()) {
+                case WRITE:
+                    executeWrite(session, dataOperation);
+                    break;
+                default:
+                    break;
+                }
+                tr.commit();
+            } catch (Exception e) {
+                tr.rollback();
+                throw new SpaceSynchronizationEndpointException(
+                        "Failed to execute bulk operation, latest object", e);
+            } finally {
+                if (session.isOpen()) {
+                    session.close();
+                }
+            }
         }
+
+    }
+
+    private void executeWrite(Session session, KafkaDataOperation dataOperation) {
+        if (!dataOperation.hasDataAsObject()) {
+            return;
+        }
+
+        Object entry = dataOperation.getDataAsObject();
+
+        try {
+            session.saveOrUpdate(entry);
+        } catch (HibernateException e) {
+            session.merge(entry);
+        }
+
+    }
+
+    private SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 
 }
--- example/feeder/src/main/java/com/epam/feeder/Feeder.java
+++ example/feeder/src/main/java/com/epam/feeder/Feeder.java
@@ -1,41 +1,42 @@
 package com.epam.feeder;
 
-import com.epam.common.Data;
-
-import org.openspaces.core.GigaSpace;
-import org.openspaces.core.SpaceInterruptedException;
-import org.openspaces.core.context.GigaSpaceContext;
-
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.SpaceInterruptedException;
+import org.openspaces.core.context.GigaSpaceContext;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import com.epam.common.Data;
+
 /**
- * A feeder bean starts a scheduled task that writes a new Data objects to the space
- * (in an unprocessed state).
- *
- * <p>The space is injected into this bean using OpenSpaces support for @GigaSpaceContext
+ * A feeder bean starts a scheduled task that writes a new Data objects to the
+ * space (in an unprocessed state).
+ * 
+ * <p>
+ * The space is injected into this bean using OpenSpaces support for @GigaSpaceContext
  * annotation.
- *
- * <p>The scheduling uses the java.util.concurrent Scheduled Executor Service. It
+ * 
+ * <p>
+ * The scheduling uses the java.util.concurrent Scheduled Executor Service. It
  * is started and stopped based on Spring lifecycle events.
  */
 public class Feeder implements InitializingBean, DisposableBean {
 
-    Logger log= Logger.getLogger(this.getClass().getName());
-    
+    Logger log = Logger.getLogger(this.getClass().getName());
+
     private ScheduledExecutorService executorService;
 
     private ScheduledFuture<?> sf;
 
     private long numberOfTypes = 10;
 
-    private long defaultDelay = 1000;
+    private long defaultDelay = 10000;
 
     private FeederTask feederTask;
 
@@ -43,10 +44,12 @@ public class Feeder implements InitializingBean, DisposableBean {
     private GigaSpace gigaSpace;
 
     /**
-     * Sets the number of types that will be used to set {@link org.openspaces.example.data.common.Data#setType(Long)}.
-     *
-     * <p>The type is used as the routing index for partitioned space. This will affect the distribution of Data
-     * objects over a partitioned space.
+     * Sets the number of types that will be used to set
+     * {@link org.openspaces.example.data.common.Data#setType(Long)}.
+     * 
+     * <p>
+     * The type is used as the routing index for partitioned space. This will
+     * affect the distribution of Data objects over a partitioned space.
      */
     public void setNumberOfTypes(long numberOfTypes) {
         this.numberOfTypes = numberOfTypes;
@@ -56,33 +59,36 @@ public class Feeder implements InitializingBean, DisposableBean {
         this.defaultDelay = defaultDelay;
     }
 
+    @Override
     public void afterPropertiesSet() throws Exception {
         log.info("--- STARTING FEEDER WITH CYCLE [" + defaultDelay + "]");
         executorService = Executors.newScheduledThreadPool(1);
         feederTask = new FeederTask();
-        sf = executorService.scheduleAtFixedRate(feederTask, defaultDelay, defaultDelay,
-                TimeUnit.MILLISECONDS);
+        sf = executorService.scheduleAtFixedRate(feederTask, defaultDelay,
+                defaultDelay, TimeUnit.MILLISECONDS);
     }
 
+    @Override
     public void destroy() throws Exception {
         sf.cancel(false);
         sf = null;
         executorService.shutdown();
     }
-    
+
     public long getFeedCount() {
         return feederTask.getCounter();
     }
 
-    
     public class FeederTask implements Runnable {
 
         private long counter = 1;
 
+        @Override
         public void run() {
             try {
                 long time = System.currentTimeMillis();
-                Data data = new Data((counter++ % numberOfTypes), "FEEDER " + Long.toString(time));
+                Data data = new Data((counter++ % numberOfTypes), "FEEDER "
+                        + Long.toString(time));
                 gigaSpace.write(data);
                 log.info("--- FEEDER WROTE " + data);
             } catch (SpaceInterruptedException e) {
@@ -97,5 +103,4 @@ public class Feeder implements InitializingBean, DisposableBean {
         }
     }
 
-    
 }
