e046ba76e3a49827cac0fc2639552963f8a6e917
Oleksiy Dyagilev <oleksiy_dyagilev@epam.com>
2014-02-13 14:50:39 +0200

    more javadoc

--- kafka-persistence/src/main/java/com/epam/openspaces/persistency/kafka/KafkaPersistenceConstants.java
+++ kafka-persistence/src/main/java/com/epam/openspaces/persistency/kafka/KafkaPersistenceConstants.java
@@ -5,6 +5,7 @@ package com.epam.openspaces.persistency.kafka;
  */
 public class KafkaPersistenceConstants {
 
+    /** Name of SpaceDocument property that determines associated Kafka topic **/
     public static final String SPACE_DOCUMENT_KAFKA_TOPIC_PROPERTY_NAME = "kafka.topic";
 
 }
